e1c106cb3fda681c52249cbb3fc35a47b9d8155f
jurgen.kluft <jurgen.kluft@gmail.com>
2025-05-19 19:20:33 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -21,15 +21,15 @@ func GetPackage() *denv.Package {
 	mainpkg.AddPackage(cbasepkg)
 
 	// 'chistogram' library
-	mainlib := denv.SetupDefaultCppLibProject("chistogram", "github.com\\jurgen-kluft\\chistogram")
-	mainlib.Dependencies = append(mainlib.Dependencies, cfilepkg.GetMainLib())
-	mainlib.Dependencies = append(mainlib.Dependencies, cbasepkg.GetMainLib())
+	mainlib := denv.SetupCppLibProject("chistogram", "github.com\\jurgen-kluft\\chistogram")
+	mainlib.AddDependencies(cfilepkg.GetMainLib()...)
+	mainlib.AddDependencies(cbasepkg.GetMainLib()...)
 
 	// 'chistogram' unittest project
 	maintest := denv.SetupDefaultCppTestProject("chistogram_test", "github.com\\jurgen-kluft\\chistogram")
-	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, cfilepkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
+	maintest.AddDependencies(cunittestpkg.GetMainLib()...)
+	maintest.AddDependencies(cfilepkg.GetMainLib()...)
+	maintest.AddDependencies(cbasepkg.GetMainLib()...)
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
